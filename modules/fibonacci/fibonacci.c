fd21863cb32fa7261b15d92887ee62e0454c7a3a
MeltedPenguin <diablo22007@gmail.com>
2018-06-10 20:54:01 +0200

    Add first fibonacci poc module

---
+++ modules/fibonacci/fibonacci.c
@@ -0,0 +1,12 @@
+#include <emscripten.h>
+
+EMSCRIPTEN_KEEPALIVE
+int fibonacci(int n) {
+  int i, t, a = 0, b = 1;
+  for (i = 0; i < n; i++) {
+    t = a + b;
+    a = b;
+    b = t;
+  }
+  return b;
+}
